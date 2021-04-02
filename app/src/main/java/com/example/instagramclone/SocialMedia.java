9e919f4a5f1d38b6ebec5535105539bb994dd96a
LASHEB MOHAMED AMINE <ma.lasheb@esi-sba.dz>
2021-04-02 16:39:54 +0100

    Login & LogOut ,ProgressDialog ,InputMethodManager(debbug) ,SocialMediaActivityAdding ,tabAddapter ( Profile ,Users ,SharePictures ) ,Update Profile's Info in Server Profile Name,Bio.. ,Adding List of users ,SharePage ,Upload image to Serv(test)

--- app/src/main/java/com/example/instagramclone/SocialMedia.java
+++ app/src/main/java/com/example/instagramclone/SocialMedia.java
@@ -1,19 +1,42 @@
 package com.example.instagramclone;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toolbar;
 
 import com.google.android.material.tabs.TabLayout;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.io.ByteArrayOutputStream;
+import java.net.URI;
 
 public class SocialMedia extends AppCompatActivity {
 
-    private ViewPager viewPager ;
-    private androidx.appcompat.widget.Toolbar  toolbar;
-    private TabLayout tabLayout ;
-    private tabAdapter tabAdapter ;
+    private ViewPager viewPager;
+    private androidx.appcompat.widget.Toolbar toolbar;
+    private TabLayout tabLayout;
+    private tabAdapter tabAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +44,91 @@ public class SocialMedia extends AppCompatActivity {
         setContentView(R.layout.activity_social_media);
 
         setTitle("Social Media App !!!");
-        toolbar=findViewById(R.id.toolbar) ;
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        viewPager = findViewById(R.id.viewPager) ;
-        tabAdapter = new tabAdapter(getSupportFragmentManager()) ;
+        viewPager = findViewById(R.id.viewPager);
+        tabAdapter = new tabAdapter(getSupportFragmentManager());
         viewPager.setAdapter(tabAdapter);
 
-        tabLayout = findViewById(R.id.TabLayout) ;
-            tabLayout.setupWithViewPager(viewPager,false);
+        tabLayout = findViewById(R.id.TabLayout);
+        tabLayout.setupWithViewPager(viewPager, false);
 
 
     }
-}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.my_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.postImageItem) {
+            if (Build.VERSION.SDK_INT >= 23 &&
+                    checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.READ_EXTERNAL_STORAGE}, 3000);
+            } else {
+                getChosenImage();
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 3000) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getChosenImage();
+            }
+        }
+    }
+
+    private void getChosenImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 4000);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ((requestCode == 4000) && (resultCode == RESULT_OK) && (data != null)) {
+            try {
+                Uri capturedImage = data.getData();
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), capturedImage);
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+                byte[] bytes = byteArrayOutputStream.toByteArray();
+
+                ParseFile parseFile = new ParseFile("img.png", bytes);
+                ParseObject parseObject = new ParseObject("Photo");
+                parseObject.put("Picture", parseFile);
+                parseObject.put("username", ParseUser.getCurrentUser().getUsername());
+                final ProgressDialog dialog = new ProgressDialog(this);
+                dialog.show();
+                parseObject.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            FancyToast.makeText(SocialMedia.this, "UPLOAD SUCCESS", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                        } else {
+                            FancyToast.makeText(SocialMedia.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+                        }
+
+                    dialog.dismiss() ;} }
+                );
+
+                } catch(Exception e){
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
